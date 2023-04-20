1ca034e6f159d567f527cf37fa3acdb8ea60ef5e
David Love <david.love@leedsbeckett.ac.uk>
2023-04-20 15:47:18 +0100

    Add sanity checks to the documentation and code using ruff

--- bin/hook.sh
+++ bin/hook.sh
@@ -3,5 +3,15 @@
 # Reformat the code into the correct style
 black .
 
-# Check for obvious errors/style violations
-flake8
+# Check the code for obvious errors/style
+# violations
+ruff check .
+
+# Check for less obvious errors using 'mypy. We don't do strict (yet)
+# as the MicroPython parser has problems with some of the needed type
+# notation
+mypy lbutils
+
+# Check the documentation for sanity and conformance
+# to the house style
+docformatter --in-place --config ./pyproject.toml .
